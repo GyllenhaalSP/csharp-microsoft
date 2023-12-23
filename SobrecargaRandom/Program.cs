8cb76e888b22e75fd0dc38206265c68b0dadebb3
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-12-23 13:14:53 +0100

    Primeras prácticas curso básico Microsoft.

---
+++ SobrecargaRandom/Program.cs
@@ -0,0 +1,8 @@
+﻿Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"Primera tirada: {roll1}");
+Console.WriteLine($"Segunda tirada: {roll2}");
+Console.WriteLine($"Tercera tirada: {roll3}");
