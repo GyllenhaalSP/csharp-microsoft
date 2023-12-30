3eee4ae5d6f714ac40e571e4e9290c4146283b99
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-12-30 02:00:27 +0100

    Siguientes partes.

---
+++ 006PedidosFraudulentos/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] pedidos = new string[] { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+foreach (string pedido in pedidos)
+{
+    if (pedido.StartsWith("B"))
+    {
+        Console.WriteLine(pedido);
+    }
+}
