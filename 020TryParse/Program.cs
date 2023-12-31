4273058aa3ea65392ee22785d21790a266497147
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-12-31 10:45:37 +0100

    Avanzando...

---
+++ 020TryParse/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] values = { "12,3", "45", "ABC", "11", "DEF" };
+decimal total = 0;
+string message = "";
+
+foreach (string value in values)
+{
+    if (decimal.TryParse(value, out decimal number))
+    {
+        total += number;
+        //Console.WriteLine($"'{value}' added to total ({total})");
+    }
+    else
+    {
+        message += value;
+    }
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
---
+++ 022PangramReversal/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.Tracing;
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+string reversedPangram = "";
+foreach (string word in pangram.Split(' '))
+{
+    string reverseWord = "";
+    for (int i = word.Length - 1; i >= 0; i--)
+    {
+        reverseWord += word[i];
+    }
+    reversedPangram += reverseWord + " ";
+}
+
+Console.WriteLine(reversedPangram);
